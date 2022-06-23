f7ad37652964aefa7abf7842738154fd78c42d75
canogg <88619478+canogg@users.noreply.github.com>
2022-06-23 10:47:44 -0300

    :lipstick: feat: Adicionando view e controller profile

--- Illustra/Illustra/Controller/ProfileController.swift
+++ Illustra/Illustra/Controller/ProfileController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class ProfileController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var userImage: UIImageView!
+
+    var listaImagens = ["astralunar", "makemoney", "sendingcut", "techna"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userImage.clipsToBounds = true
+        userImage.layer.cornerRadius = userImage.frame.height / 2
     }
-    
 
     /*
     // MARK: - Navigation
@@ -25,5 +29,40 @@ class ProfileController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension ProfileController: UICollectionViewDelegate {
+}
+
+extension ProfileController: UICollectionViewDataSource {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listaImagens.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView,
+                               cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+                as? IllustrationCell else { return UICollectionViewCell() }
+        cell.image.image = UIImage(named: listaImagens[indexPath.row])
+        return cell
+    }
+}
+
+class IllustrationCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+}
+
+extension ProfileController: UITableViewDelegate {
+}
+
+extension ProfileController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
 }
